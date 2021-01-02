415cdcc690b4c649428a9bc171ada8d1e85cef05
Dan Tulovsky <dant@wetsnow.com>
2021-01-02 12:02:52 -0500

    check roles

--- server/server/auth.go
+++ server/server/auth.go
@@ -15,7 +15,8 @@ import (
 )
 
 var (
-	cloakClient = gocloak.NewClient(oidcProviderURL)
+	cloakClient   = gocloak.NewClient(oidcProviderURL)
+	expectedRoles = []string{"user"}
 )
 
 const (
@@ -23,9 +24,67 @@ const (
 	oidcProviderURL = "https://login.wetsnow.com"
 	realm           = "wetsnow"
 	audience        = "pepper-poker-grpc"
+	client          = "pepper-poker-grpc.wetsnow.com"
 )
 
+func roleInList(role string, roles []string) bool {
+	for _, r := range roles {
+		if role == r {
+			return true
+		}
+	}
+	return false
+}
+
+func haveRoles(expected, have []string) bool {
+	for _, role := range expected {
+		if !roleInList(role, have) {
+			return false
+		}
+	}
+	return true
+}
+
 type uinfoType string
+
+func validateRoles(claims jwt.MapClaims) error {
+	var resourceAccess map[string]interface{}
+	var ok bool
+
+	if resourceAccess, ok = claims["resource_access"].(map[string]interface{}); !ok {
+		return fmt.Errorf("resource_access key missing")
+	}
+
+	for key, value := range resourceAccess {
+		log.Printf("%#v: %#v", key, value)
+	}
+
+	var clientMap map[string]interface{}
+
+	if clientMap, ok = resourceAccess[client].(map[string]interface{}); !ok {
+		return fmt.Errorf("client key missing")
+	}
+
+	for key, value := range clientMap {
+		log.Printf("%#v (%T): %#v", key, key, value)
+	}
+
+	var roles []interface{}
+	if roles, ok = clientMap["roles"].([]interface{}); !ok {
+		return fmt.Errorf("roles key missing")
+	}
+
+	var myRoles []string
+	for _, role := range roles {
+		log.Printf("> %v", role.(string))
+		myRoles = append(myRoles, role.(string))
+	}
+
+	if !haveRoles(expectedRoles, myRoles) {
+		return fmt.Errorf("missing required roles: %v", expectedRoles)
+	}
+	return nil
+}
 
 func validateToken(ctx context.Context, token, realm string) (*jwt.Token, error) {
 
@@ -37,6 +96,7 @@ func validateToken(ctx context.Context, token, realm string) (*jwt.Token, error)
 	log.Printf("%#v", t)
 	log.Printf("%#v", claims)
 
+	// This calls out to the server
 	log.Printf("retrospecting token...")
 	res, err := cloakClient.RetrospectToken(ctx, token, "pepper-poker.wetsnow.com", "b24e6370-2c12-44d2-85db-43fb79ab3382", "wetsnow")
 	if err != nil {
@@ -48,11 +108,26 @@ func validateToken(ctx context.Context, token, realm string) (*jwt.Token, error)
 		return nil, fmt.Errorf("provided access token not valid")
 	}
 
-	return t, nil
-}
+	vHelper := jwt.NewValidationHelper(jwt.WithAudience(audience))
 
-func userClaimFromToken(struct{}) string {
-	return "foobar"
+	// offline validation (this does almost no validation by default)
+	if err := claims.Valid(vHelper); err != nil {
+		log.Print(err)
+		spew.Dump(claims)
+		return nil, err
+	}
+
+	// TODO: Create custom validator to check roles?
+	for key, value := range *claims {
+		log.Printf("%v: %v", key, value)
+
+	}
+
+	if err := validateRoles(*claims); err != nil {
+		return nil, err
+	}
+
+	return t, nil
 }
 
 // pokerAuthFunc is used by a middleware to authenticate requests
@@ -81,6 +156,8 @@ func pokerAuthFunc(ctx context.Context) (context.Context, error) {
 	grpc_ctxtags.Extract(ctx).Set("auth.sub", uinfo.Sub)
 
 	newCtx := context.WithValue(ctx, uinfoType("uinfo"), uinfo)
+
+	// claims := userClaimFromToken(token)
 
 	return newCtx, nil
 }
